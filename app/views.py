2ffee9a83e775968eb8bf4fb65f5c56fa3f1f713
Ethan McDowell <mcdowele@c02yt83glvcg.wwt.com>
2020-06-24 09:18:53 -0500

    Adds openshift template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, url_for, redirect, g, abort
 from app import app
 from .forms import TaskForm
+import os
 
+# Function that adds a task to the to-do list
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     form = TaskForm()
@@ -11,13 +13,11 @@ def index():
     selection = list(r.table('todos').run(g.rdb_conn))
     return render_template('index.html', form=form, tasks=selection)
 
+# Function that deletes a task from the to-do list
 @app.route("/delete/<string:task_id>")
 def delete_task(task_id):
-    #form = TaskForm()
     r.table('todos').filter({"id": task_id}).delete().run(g.rdb_conn)
     return redirect(url_for('index'))
-    #selection = list(r.table('todos').run(g.rdb_conn))
-    #return render_template('index.html', form=form, tasks=selection)
 
 
 # rethink imports
@@ -26,9 +26,11 @@ from rethinkdb.errors import RqlRuntimeError
 
 # rethink config
 r = rdb.RethinkDB()
-RDB_HOST = 'localhost'
+RDB_HOST = os.getenv('DATABASE_SERVICE_NAME')
 RDB_PORT = 28015
-LISTY_DB = 'listy'
+LISTY_DB = os.getenv('DATABASE_NAME')
+RBD_USER = os.getenv('DATABASE_USER')
+RDB_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
 # db setup; only run once
 def dbSetup():
@@ -47,7 +49,7 @@ dbSetup()
 @app.before_request
 def before_request():
     try:
-        g.rdb_conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=LISTY_DB)
+        g.rdb_conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=LISTY_DB, user=RBD_USER, password=RDB_PASSWORD)
     except RqlRuntimeError:
         abort(503, 'Database connection could not be established.')
 
